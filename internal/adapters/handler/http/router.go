adb1a333aae69bb5dde022c1a23b8c0892bcd2d8
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2026-02-15 15:23:18 +0100

    fix(app): allow X-Timezone header in CORS configuration

--- internal/adapters/handler/http/router.go
+++ internal/adapters/handler/http/router.go
@@ -33,7 +33,7 @@ func NewRouter(deps RouterDependencies) *gin.Engine {
 	router.Use(func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Timezone")
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
